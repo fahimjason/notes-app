97507f3f842f100ed0125d2acc83ea01cc64b4eb
JisanFahim <JisanFahim@users.noreply.github.com>
2021-12-20 01:00:10 +0600

    test done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const chalk = require('chalk')
 const yargs = require('yargs')
 const notes = require('./notes.js')
 
-// Customize yargs version test text
+// Customize yargs version
 yargs.version('1.1.0')
 
 // Create add command
